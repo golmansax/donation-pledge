105b70530c774548725a04a35800cc68d3439388
Holman Gao <holman@golmansax.com>
2015-10-02 15:25:47 -0700

    Ug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if (process.env.NODE_ENV === 'development') { require('dotenv').load(); }
+const env = process.env.NODE_ENV || 'development';
+if (env === 'development') { require('dotenv').load(); }
 
 require('babel/register');
 require('./server').listen(process.env.PORT || 3000);
