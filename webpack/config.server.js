9a9825fade6075632b43409e705713a103a23631
Holman Gao <holman@golmansax.com>
2015-10-26 22:56:43 -0700

    Fix lint

--- webpack/config.server.js
+++ webpack/config.server.js
@@ -42,7 +42,7 @@ module.exports = {
 
   plugins: [
     new ExtractTextPlugin(`[name].${isProduction() ? '.[chunkhash]' : ''}.css`, {
-      allChunks: true
+      allChunks: true,
     }),
     new AssetsPlugin({
       path: path.join(__dirname, 'build'),
