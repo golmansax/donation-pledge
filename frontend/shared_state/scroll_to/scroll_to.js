ea97edc6218a2d42616c5e5fed16cfa3c06bb5cd
Holman Gao <holman@golmansax.com>
2015-10-30 00:18:13 -0700

    Remove dead file

--- frontend/shared_state/scroll_to/scroll_to.js
+++
@@ -1,9 +0,0 @@
-const storage = {};
-
-export function registerScrollTarget(targetKey, target) {
-  if (targetKey in storage) {
-    throw new Error(`Target '${targetKey}' has already been registered`);
-  }
-
-  storage[targetKey] = target;
-}
