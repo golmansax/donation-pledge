e0d60f66383b8a27034a4b24784820c8a4a64297
Holman Gao <holman@golmansax.com>
2015-11-19 10:41:23 -0800

    Gah work around

--- frontend/base/utils/make_server_entry.js
+++ frontend/base/utils/make_server_entry.js
@@ -2,7 +2,9 @@ import { PropTypes } from 'react';
 import { loadAssets } from '../../shared_state/asset_store';
 
 export default function makeServerEntry(MyComponent) {
-  const ServerComponent = ({ assets, ...otherProps}) => {
+  /* eslint-disable no-redeclare */
+  const ServerComponent = ({ assets, ...otherProps }) => {
+    /* eslint-enable no-redeclare */
     loadAssets(assets);
     return <MyComponent {...otherProps} />;
   };
