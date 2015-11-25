72337d20c29166571d670a798858e0ad39718454
Holman Gao <holman@golmansax.com>
2015-11-24 22:39:40 -0800

    Fix lint

--- frontend/base/utils/make_client_entry.js
+++ frontend/base/utils/make_client_entry.js
@@ -1,7 +1,5 @@
-import { PropTypes } from 'react';
 import { render } from 'react-dom';
 import { PopupBackdrop } from '../../shared_state/popup/components';
-import { loadAssets } from '../../shared_state/asset_store';
 
 export default function makeClientEntry(MyComponent) {
   render(<PopupBackdrop />, document.getElementById('popupBackdrop'));
