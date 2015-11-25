a408c4565f8101bf3fc04935ee5432b9d4099df5
Holman Gao <holman@golmansax.com>
2015-11-24 21:06:09 -0800

    Update syntax

--- frontend/donation/components/list.js
+++ frontend/donation/components/list.js
@@ -2,30 +2,26 @@ import { Component, PropTypes } from 'react';
 import { A, P } from '../../base/components';
 import styles from './list.styl';
 
-export default class DonationList extends Component {
-  render() {
-    return (
-      <div>{this.props.donations.map(this._renderDonation)}</div>
-    );
-  }
-
-  _renderDonation(donation, index) {
-    return (
-      <div className={styles.listItem} key={index}>
-        <div className={styles.year}>{donation.year}</div>
-        <div className={styles.content}>
-          <div className={styles.heading}>
-            <span className={styles.action}>Donated</span>
-            &nbsp;to&nbsp;
-            <A href={donation.organization.url}>{donation.organization.text}</A>
-          </div>
-          <P type='secondary'>{donation.reason}</P>
-        </div>
+const renderDonation = (donation, index) => (
+  <div className={styles.listItem} key={index}>
+    <div className={styles.year}>{donation.year}</div>
+    <div className={styles.content}>
+      <div className={styles.heading}>
+        <span className={styles.action}>Donated</span>
+        &nbsp;to&nbsp;
+        <A href={donation.organization.url}>{donation.organization.text}</A>
       </div>
-    );
-  }
-}
+      <P type='secondary'>{donation.reason}</P>
+    </div>
+  </div>
+);
+
+const DonationList = ({ donations }) => (
+  <div>{donations.map(renderDonation)}</div>
+);
 
 DonationList.propTypes = {
   donations: PropTypes.array.isRequired,
 };
+
+export default DonationList;
