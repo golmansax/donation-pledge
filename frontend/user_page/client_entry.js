4edf17d5dd24d1a1b9176411b4134476f2730175
Holman Gao <holman@golmansax.com>
2015-12-21 13:08:06 -0800

    Fix pledger page

--- frontend/user_page/client_entry.js
+++ frontend/user_page/client_entry.js
@@ -1,8 +1,27 @@
 import { render } from 'react-dom';
+import PLEDGERS from '_data/pledgers';
 import { UserPageContent } from './components';
 import { makeClientEntry } from '../base/utils';
 
-render(
-  makeClientEntry(<UserPageContent />),
-  document.getElementById('content')
-);
+const getPledgerIdFromUrl = () => {
+  const matches = window.location.href.match(/\/pledgers\/([a-zA-Z\d\-]*)/);
+  if (!matches) { return null; }
+  return matches[1];
+};
+
+const getPledger = () => {
+  const pledgerId = getPledgerIdFromUrl();
+  if (!pledgerId) { return null; }
+  if (!PLEDGERS.hasOwnProperty(pledgerId)) { return null; }
+  return PLEDGERS[pledgerId];
+};
+
+const pledger = getPledger();
+
+// If we can't find the pledger, don't render anything
+if (pledger) {
+  render(
+    makeClientEntry(<UserPageContent pledger={pledger} />),
+    document.getElementById('content')
+  );
+}
